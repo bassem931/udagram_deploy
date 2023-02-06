34028bbf9a1a23b38caabe9bcf47bbf274e94504
bassem931 <bti.bassem@gmail.com>
2023-02-06 17:41:41 +0200

    fixed frontend linting errors

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,13 +4,12 @@
 
 export const environment = {
   production: false,
-  appName: "Udagram",
-  apiHost: "http://udagram-dev22222.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-dev22222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 // local run
-//apiHost: "http://localhost:8080/api/v0",
-
+// apiHost: "http://localhost:8080/api/v0",
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
