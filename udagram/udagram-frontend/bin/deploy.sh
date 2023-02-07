b7d9e37d9bd8761e554ba5c9b0a01e2ad34885c2
bassem931 <bti.bassem@gmail.com>
2023-02-07 17:30:01 +0200

    added setenv line in deploy.sh script

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,4 @@
 aws s3 cp --recursive --acl public-read ./www s3://udagramfrontbucket/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagramfrontbucket/
+
+eb setenv PORT=$PORT USERNAME=$USERNAME
